85b4aad9d26e8fd60b9908b781544dd695eb01c6
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2023-05-24 08:49:03 +0400

    Is added validate_email,validate_phone,validate_full_name method for checking email,phone and full_name is True or Not

--- schemas/request/user.py
+++ schemas/request/user.py
@@ -6,9 +6,26 @@ from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from pydantic import BaseModel, EmailStr, SecretStr, validator
 
 
+# ?Third Party packages for FastApi
+from email_validator import validate_email as validate_user_email, EmailNotValidError
+
+
+# ?Pyhon modules and Functions
+import re
+
+
 #!UserBase
 class UserBase(BaseModel):
     email: str
+
+    # validate_email
+    @validator("email")
+    def validate_email(cls, email):
+        try:
+            validate_user_email(email)
+            return email
+        except EmailNotValidError:
+            raise ValueError("Email is not valid format")
 
 
 # *UserRegisterIn
@@ -19,6 +36,24 @@ class UserRegisterIn(UserBase):
     last_name: str
     full_name: str
     iban: str
+
+    # validate_phone
+    @validator("phone")
+    def validate_phone(cls, phone):
+        regex_pattern = "994\s?\d{2}[2-9]\d{6}"
+        if re.match(regex_pattern, phone):
+            return phone
+        else:
+            raise ValueError("Phone number must be in this format:994xxxxxxxxx'")
+
+    # validate_full_name
+    @validator("full_name")
+    def validate_full_name(cls, full_name):
+        try:
+            first_name, last_name = full_name.split()
+            return full_name
+        except Exception as ex:
+            raise ValueError("You should provide at least first_name and last_name")
 
 
 # *UserLoginIn
