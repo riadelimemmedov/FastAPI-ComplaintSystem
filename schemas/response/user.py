6a6b6b6e9f0ab5ef4e805273d766cf0c6646453d
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2023-05-30 13:59:55 +0400

    Added new field to UserOut class when back to response to client showing specifig field to UI

--- schemas/response/user.py
+++ schemas/response/user.py
@@ -1,13 +1,14 @@
-# ?Models,Schemas and Serializers
+
+#?Models,Schemas and Serializers
 from models import RoleType
 from schemas.base import UserBase
 
 
 #!UserOut
 class UserOut(UserBase):
-    id: int
-    first_name: str
-    last_name: str
-    phone: str
-    role: RoleType
-    iban: str
+    id:int
+    first_name : str
+    last_name : str
+    phone : str
+    role : RoleType
+    iban : str
