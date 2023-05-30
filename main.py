f83803ce098990a25de4ce49574f6820fdc81b9c
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2023-05-30 13:54:13 +0400

    Added Cors middleware to this app

--- main.py
+++ main.py
@@ -2,15 +2,29 @@
 
 # ?FastApi
 from fastapi import APIRouter, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.cors import CORSMiddleware
 
 # ?Database properties
 from db import database
+
 # ?Custom router
 from resources.routes import api_router
+
+# ?Here we define CORS for connnet backend application to frontend
+origins = ["http://127.0.0.1:8000/", "http://localhost"]
+
 
 # Create FastApi object from FastAPI class
 app = FastAPI()
 app.include_router(api_router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[origins],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 #!startup
